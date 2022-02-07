4d3668c23d8b9682ce1102311457312627e44170
Michael Evko <mbevko@gmail.com>
2022-02-07 13:05:22 -0500

    added header logo and css

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <img
+        src="../src/css/westreet-liquor-company-logo-black.png"
+        class="header_logo"
+      />
+    </header>
+  );
+}
