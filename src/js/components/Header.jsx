db30220d88da5c7dcbf0b484317ade4055faec4d
Michael Evko <mbevko@gmail.com>
2022-02-09 12:47:01 -0500

    added logout button -no functionality minimum header style

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,26 +1,13 @@
 import React from "react";
 import logo_black from "../../css/images/logo_black.png";
-import "../../css/header.scss";
-import { useAuth0 } from "@auth0/auth0-react";
+import "../../css/main.scss";
 import Button from "@mui/material/Button";
 
 export default function Header() {
-  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } =
-    useAuth0();
-
-  if (isLoading) {
-    return <div>Loading ...</div>;
-  }
-
   return (
-    <div>
+    <div className="header">
       <img src={logo_black} className="header_logo" />
-      <AppShell user={user} logout={logout} />) : ( // add footer with logout
-      button
-      <Button variant="contained" onClick={() => loginWithRedirect()}>
-        Log In
-      </Button>
-      );
+      <Button variant="contained">Logout</Button>
     </div>
   );
 }
