b60638015c9d2f9b740444af24d8303b7f4b83cc
Michael Evko <mbevko@gmail.com>
2022-02-15 14:04:11 -0500

    cleaned up code and files

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import Header from "./Header";
-import Data from "./data/data.json";
 
 export default function Home() {
   // fetch from API and render
   return (
     <>
       <Header />
-      <div className="invoices">
-        {Data.map((post) => {
-          return (
-            <>
-              <h4>{post.title}</h4>
-              <p> {post.content} </p>
-            </>
-          );
-        })}
-      </div>
     </>
   );
 }
