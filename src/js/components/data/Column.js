2824ed75771c552f12fcd03a6ee6211ea8f151ae
Michael Evko <mbevko@gmail.com>
2022-02-25 14:58:48 -0500

    cleaned up code created seperate table for data

---
+++ src/js/components/data/Column.js
@@ -0,0 +1,32 @@
+const columnsOne = [
+  {
+    title: "Customer",
+    field: "customer",
+    filterPlaceholder: "Filter Customer",
+  },
+  { title: "Po Number", field: "poNumber", filterPlaceholder: "Filter PO" },
+  {
+    title: "Item Number",
+    field: "itemNumber",
+    filterPlaceholder: "Filter Item",
+  },
+  {
+    title: "Description",
+    field: "description",
+    filterPlaceholder: "Filter Description",
+  },
+  {
+    title: "Quantity",
+    field: "quantity",
+    filterPlaceholder: "Filter Quantity",
+  },
+  { title: "Unit of Measure", field: "uom", filterPlaceholder: "Filter UOM" },
+  {
+    title: "Due Date",
+    field: "date",
+    type: "date",
+    filterPlaceholder: "Filter Date",
+  },
+];
+
+export default columnsOne;
--- src/js/components/data/Rows.js
+++ src/js/components/data/Rows.js
@@ -55,33 +55,4 @@ const tableOne = [
   },
 ];
 
-const columnsOne = [
-  {
-    title: "Customer",
-    field: "customer",
-    filterPlaceholder: "Filter Customer",
-  },
-  { title: "Po Number", field: "poNumber", filterPlaceholder: "Filter PO" },
-  {
-    title: "Item Number",
-    field: "itemNumber",
-    filterPlaceholder: "Filter Item",
-  },
-  {
-    title: "Description",
-    field: "description",
-    filterPlaceholder: "Filter Description",
-  },
-  {
-    title: "Quantity",
-    field: "quantity",
-    filterPlaceholder: "Filter Quantity",
-  },
-  { title: "Unit of Measure", field: "uom", filterPlaceholder: "Filter UOM" },
-  {
-    title: "Due Date",
-    field: "date",
-    type: "date",
-    filterPlaceholder: "Filter Date",
-  },
-];
+export default tableOne;
