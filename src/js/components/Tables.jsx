f6fd1403b6f1f561c72bd7579c4a71ff482b2250
Michael Evko <mbevko@gmail.com>
2022-03-02 12:46:58 -0500

    added toolbar

--- src/js/components/Tables.jsx
+++ src/js/components/Tables.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { DataGrid } from "@mui/x-data-grid";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
 import columnsOne from "./data/Column";
 import tableOne from "./data/Rows";
 
@@ -15,39 +15,19 @@ export default function Tables() {
           rowsPerPageOptions={[5]}
           checkboxSelection
           disableSelectionOnClick
+          components={{ Toolbar: GridToolbar }}
         />
       </div>
     </>
   );
 }
 
+//data grid filters show without hovering
+//add tool bar
+//style tables
 //add download as csv
 //add colummn resize
 
 //remember different people see different things when they log on
 //expand pages
 //have previews and possible graphs on home page
-
-/*  <div className="table_one">
-      <DataGrid
-        columns={columnsOne}
-        rows={tableOne}
-        autoHeight={true}
-        options={{
-          searchFieldVariant: "outlined",
-          exportButton: true,
-          exportallData: true,
-          headerStyle: {
-            backgroundColor: "#01579b",
-            color: "#FFF",
-          },
-          grouping: true,
-          columnsButton: true,
-          paging: true,
-          pageSizeOptions: [, 5, 10, 20, 50, 100, 10000],
-          rowStyle: (data, index) =>
-            index % 2 === 0 ? { background: "#E8F6FD" } : null,
-        }}
-        title="Open Purchase Orders"
-      />
-    </div> */
