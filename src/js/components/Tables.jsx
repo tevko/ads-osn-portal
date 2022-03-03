59031843f5e99df9b236af14c333801d4a260544
Michael Evko <mbevko@gmail.com>
2022-03-02 20:04:31 -0500

    added table title

--- src/js/components/Tables.jsx
+++ src/js/components/Tables.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import { Typography } from "@mui/material";
 import columnsOne from "./data/Column";
 import tableOne from "./data/Rows";
 
@@ -7,6 +8,9 @@ export default function Tables() {
   const [pageSize, setPageSize] = React.useState(25);
   return (
     <>
+      <Typography variant="h5" style={{ color: "#383D41" }}>
+        Open Purchase Orders
+      </Typography>
       <div style={{ height: 680, width: "100%" }}>
         <DataGrid
           autoheight
