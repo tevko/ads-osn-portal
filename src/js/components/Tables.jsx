4596e72643fbc2327146cc6be175875c4098ad7c
tevko <timevko@gmail.com>
2022-03-10 14:29:22 -0600

    add invoices

--- src/js/components/Tables.jsx
+++ src/js/components/Tables.jsx
@@ -4,18 +4,18 @@ import { Typography } from "@mui/material";
 import columnsOne from "./data/Column";
 import tableOne from "./data/Rows";
 
-export default function Tables() {
+export default function Tables({ title, rows, columns }) {
   const [pageSize, setPageSize] = React.useState(25);
   return (
     <>
       <Typography variant="h5" style={{ color: "#383D41" }}>
-        Open Purchase Orders
+        {title}
       </Typography>
       <div style={{ height: 680, width: "100%" }}>
         <DataGrid
           autoheight
-          rows={tableOne}
-          columns={columnsOne}
+          rows={rows}
+          columns={columns}
           disableExtendRowFullWidth={false}
           checkboxSelection
           disableSelectionOnClick
