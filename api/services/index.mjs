b448f934ff5dc1b7fc6400d7e472838b3c2dd82d
tevko <timevko@gmail.com>
2022-03-10 23:38:16 -0600

    remove query

--- api/services/index.mjs
+++ api/services/index.mjs
@@ -19,10 +19,8 @@ const getTableName = (scope) => {
 //builds a query for SQL given a scope and a query param
 const buildQuery = (scope, queryParam) => {
   let query = `SELECT * FROM ${getTableName(scope)}`;
-  if (queryParam.id && scope !== "purchase-orders") {
-    query += ` WHERE VDCODE = '${queryParam.id}'`;
-  } else if (scope === "purchase-orders") {
-    query += ` WHERE Expr2 = '${queryParam.id}'`;
+  if (queryParam.id) {
+    //query += ` WHERE VDCODE = '${queryParam.id}'`;
   }
   // more clauses will be added here including pagination
   return query;
