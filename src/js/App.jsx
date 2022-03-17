562ee339f90561f8c5c70a50139f927d860794e4
tevko <timevko@gmail.com>
2022-03-16 23:33:14 -0500

    ensure auth header present in ftch

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "@mui/material/Button";
 
 import AppShell from "./AppShell";
 
 const App = () => {
-  const { user, isAuthenticated, isLoading, loginWithRedirect, logout } =
-    useAuth0();
+  const {
+    user,
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    logout,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  useEffect(() => {
+    const f = async () => {
+      if (isAuthenticated) {
+        const token = await getAccessTokenSilently();
+        window.localStorage.setItem("_A_C_T_", token);
+      }
+    };
+    f();
+  }, []);
 
   if (isLoading) {
     return <div>Loading ...</div>;
