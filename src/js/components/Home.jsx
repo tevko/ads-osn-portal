98616b0b86b4081cd55dc635c195be5369c96e75
Michael Evko <mbevko@gmail.com>
2022-03-21 13:05:16 -0400

    added styling, added pie charts

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Graph from "./Graph";
 import Data from "./data/Data";
+import ChartData from "./data/ChartData";
+import Chart from "./Chart";
 
 export default function Home() {
   return (
-    <div>
-      <Graph data={Data} />
+    <div className="dash_page">
+      <h1>Most Relevant Data</h1>
+      <div className="pie_charts">
+        <Chart data={ChartData} />
+        <Chart data={ChartData} />
+      </div>
+      <h1>More Data</h1>
+      <div className="bar_chart">
+        <Graph data={Data} />
+      </div>
     </div>
   );
 }
---
+++ src/js/components/data/ChartData.js
@@ -0,0 +1,34 @@
+const ChartData = [
+  {
+    id: "css",
+    label: "css",
+    value: 404,
+    color: "hsl(250, 70%, 50%)",
+  },
+  {
+    id: "java",
+    label: "java",
+    value: 418,
+    color: "hsl(177, 70%, 50%)",
+  },
+  {
+    id: "haskell",
+    label: "haskell",
+    value: 505,
+    color: "hsl(241, 70%, 50%)",
+  },
+  {
+    id: "c",
+    label: "c",
+    value: 428,
+    color: "hsl(100, 70%, 50%)",
+  },
+  {
+    id: "elixir",
+    label: "elixir",
+    value: 480,
+    color: "hsl(224, 70%, 50%)",
+  },
+];
+
+export default ChartData;
