9c05355b3998e78e72c5564d378f82bfb55d41d3
tevko <timevko@gmail.com>
2022-03-23 23:36:19 -0500

    add ability to create user in admin

--- src/js/components/Admin.jsx
+++ src/js/components/Admin.jsx
@@ -2,30 +2,89 @@ import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
 
 export default function Admin(props) {
+  const [selectedUserType, setSelectedUserType] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [createUserLoading, setCreateUserLoading] = useState(false);
+
   const role = props["https://mzfweb2.adssglobal.net/api/roles"];
   if (role[0] !== "Admin") return <h1>Access Denied</h1>;
 
   const { userTypesData, userTypeserror, userTypesloading } = useFetch(
     `${window.API_BASE_URL}/user-types`
   );
-  const [selectedUserType, setSelectedUserType] = useState(null);
+
+  const createUser = (e) => {
+    e.preventDefault();
+    setCreateUserLoading(true);
+    window
+      .fetch(`${window.API_BASE_URL}/create-user`, {
+        credentials: "include",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("_A_C_T_")}`,
+        },
+        method: "POST",
+        body: JSON.stringify({
+          email,
+          password,
+          userRole: selectedUserType,
+        }),
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error)
+          alert("There was a problem creating the user. Please try again.");
+      })
+      .catch((err) => {
+        alert("There was a problem creating the user. Please try again.");
+      })
+      .finally(() => {
+        setCreateUserLoading(false);
+      });
+  };
+
   if (userTypesloading) return <p>Loading...</p>;
   if (userTypeserror) return <p>Error: {error.message}</p>;
-  // return a form that allows the user to add a new user type using userTypesData, with email and password fields
+
   return (
     <div>
       <h1>Admin</h1>
       <h2>User Types</h2>
-      <ul>
-        {(userTypesData || [{ id: 1, name: "---" }]).map((userType) => (
-          <li key={userType.id} onClick={() => setUserType(userType.name)}>
-            {userType.name}
-          </li>
-        ))}
-      </ul>
-      <input type="email" placeholder="Email Address" />
-      <input type="text" placeholder="Password" />
-      <button>Add User Type</button>
+      <form onSubmit={createUser}>
+        <select onChange={(e) => setSelectedUserType(e.target.value)}>
+          <option value="">Select a user type</option>
+          <option value="Admin">Admin</option>
+          {(userTypesData || []).map((userType) => (
+            <option key={userType.VENDORID} value={userType.VENDNAME}>
+              {userType.VENDNAME}
+            </option>
+          ))}
+        </select>
+        <input
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email Address"
+        />
+        <input
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button
+          disabled={
+            createUserLoading ||
+            !selectedUserType ||
+            !email ||
+            !password ||
+            selectedUserType === ""
+          }
+          type="submit"
+        >
+          Add User Type
+        </button>
+      </form>
     </div>
   );
 }
