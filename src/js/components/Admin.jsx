6f2f5c0029405cbb37385d293767dac7a4a2251d
Michael Evko <mbevko@gmail.com>
2022-03-26 15:48:23 -0400

    styled, added searchable dropdown box, added table

--- src/js/components/Admin.jsx
+++ src/js/components/Admin.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 import useFetch from "../hooks/useFetch";
 
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+
+import Tables from "./Tables";
+import ColumnsOne from "./data/Columns";
+import RowsOne from "./data/Rows";
+import "../../css/main.scss";
+
 export default function Admin(props) {
   const [selectedUserType, setSelectedUserType] = useState(null);
   const [email, setEmail] = useState(null);
@@ -46,33 +57,49 @@ export default function Admin(props) {
   if (userTypesloading) return <p>Loading...</p>;
   if (userTypeserror) return <p>Error: {error.message}</p>;
 
+  const Users = [
+    { label: "Select a User", value: "" },
+    { label: "Admin", value: "Admin" },
+  ];
+
   return (
-    <div>
-      <h1>Admin</h1>
-      <h2>User Types</h2>
-      <form onSubmit={createUser}>
-        <select onChange={(e) => setSelectedUserType(e.target.value)}>
-          <option value="">Select a user type</option>
-          <option value="Admin">Admin</option>
-          {(userTypesData || []).map((userType) => (
-            <option key={userType.VENDORID} value={userType.VENDNAME}>
-              {userType.VENDNAME}
-            </option>
-          ))}
-        </select>
-        <input
+    <div className="admin_page">
+      <Typography variant="h4" className="new_user_heading" color="#fff">
+        Create User
+      </Typography>
+      <form onSubmit={createUser} className="user_types_form">
+        <Autocomplete
+          disablePortal
+          id="combo-box-demo"
+          size="small"
+          options={userTypesData || Users}
+          sx={{ width: 250 }}
+          renderInput={(params) => <TextField {...params} label="Users" />}
+        />
+
+        <TextField
           required
           type="email"
+          size="small"
           onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email Address"
+          label="Email Address"
+          variant="outlined"
+          style={{ width: 250 }}
         />
-        <input
+        <TextField
           required
           type="password"
+          size="small"
           onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
+          label="Password"
+          variant="outlined"
+          style={{ width: 250 }}
         />
-        <button
+        <Button
+          color="primary"
+          sx={{ width: "100" }}
+          size="large"
+          variant="outlined"
           disabled={
             createUserLoading ||
             !selectedUserType ||
@@ -82,9 +109,13 @@ export default function Admin(props) {
           }
           type="submit"
         >
-          Add User Type
-        </button>
+          ADD USER
+        </Button>
       </form>
+      <Typography variant="h4" color="#fff">
+        Existing Users
+      </Typography>
+      <Tables rows={RowsOne} columns={ColumnsOne} />
     </div>
   );
 }
