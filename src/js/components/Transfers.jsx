33c1bdaf5e8f4719a09b785a53ae10b87a955258
Michael Evko <mbevko@gmail.com>
2022-04-13 11:59:18 -0400

    extended with of columns

--- src/js/components/Transfers.jsx
+++ src/js/components/Transfers.jsx
@@ -15,51 +15,61 @@ export default function Transfers() {
           field: "DOCNUM",
           headerName: "Document Number",
           flex: 1,
+          minWidth: 150,
         },
         {
           field: "VDCODE",
           headerName: "Vendor Code",
           flex: 1,
+          minWidth: 150,
         },
         {
           field: "VDNAME",
           headerName: "Vendor Name",
           flex: 1,
+          minWidth: 150,
         },
         {
           field: "ITEMNO",
           headerName: "Item Number",
           flex: 1,
+          minWidth: 150,
         },
         {
           field: "ITEMDESC",
           headerName: "Item Description",
           flex: 1,
+          minWidth: 150,
         },
         {
           field: "FROMLOC",
           headerName: "From Location",
           flex: 1,
+          minWidth: 150,
         },
         {
           field: "TOLOC",
           headerName: "To Location",
           flex: 1,
+          minWidth: 150,
         },
         {
           field: "QUANTITY",
           headerName: "Quantity",
           flex: 1,
+          minWidth: 150,
         },
         {
           field: "LOTNUMF",
           headerName: "Lot Number",
           flex: 1,
+          minWidth: 150,
         },
         {
           field: "UNITCOST",
           headerName: "Unit Cost",
           flex: 1,
+          minWidth: 150,
         },
       ]}
       rows={data.map((obj) => ({ ...obj, id: obj.ITEMNO + obj.DOCNUM }))}
