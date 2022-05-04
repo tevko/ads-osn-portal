b06152fd5c242542f556ea3151cc06a9a25a7267
tevko <timevko@gmail.com>
2022-05-03 22:15:15 -0500

    add tables component

--- src/js/components/Manage.jsx
+++ src/js/components/Manage.jsx
@@ -6,6 +6,7 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Tooltip from "@mui/material/Tooltip";
 
+import Tables from "./Tables";
 import "../../css/main.scss";
 
 export default function Manage(props) {
