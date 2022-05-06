6bf76ccdfffd2b557c8f3a28173bc186d56fd316
tevko <timevko@gmail.com>
2022-05-05 21:57:10 -0500

    usefetch correctly

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -4,19 +4,17 @@ import Graph from "./Graph";
 import { Typography } from "@mui/material";
 
 export default function Home() {
-  const { po, receipt, transfer, invoice, loading } = useFetch(
-    `${window.API_BASE_URL}/dashboard`
-  );
+  const { data, loading } = useFetch(`${window.API_BASE_URL}/dashboard`);
 
-  if (po && receipt && transfer && invoice) {
-    po.total = po.length;
-    po.id = "Purchase Orders";
-    receipt.total = receipt.length;
-    receipt.id = "Receipts";
-    transfer.total = transfer.length;
-    transfer.id = "Transfers";
-    invoice.total = invoice.length;
-    invoice.id = "Invoices";
+  if (data.po && data.receipt && data.transfer && data.invoice) {
+    data.po.total = data.po.length;
+    data.po.id = "Purchase Orders";
+    data.receipt.total = data.receipt.length;
+    data.receipt.id = "Receipts";
+    data.transfer.total = data.transfer.length;
+    data.transfer.id = "Transfers";
+    data.invoice.total = data.invoice.length;
+    data.invoice.id = "Invoices";
   }
 
   return loading ? (
@@ -27,7 +25,7 @@ export default function Home() {
         Totals
       </Typography>
       <div className="bar_chart">
-        <Graph data={[po, receipt, transfer, invoice]} />
+        <Graph data={[data.po, data.receipt, data.transfer, data.invoice]} />
       </div>
     </div>
   );
