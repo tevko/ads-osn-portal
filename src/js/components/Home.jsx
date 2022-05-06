491a53dcf0b2ca1218a637fa7e61fa1bc1639656
tevko <timevko@gmail.com>
2022-05-05 21:47:18 -0500

    fix dashboard

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from "react";
 import useFetch from "../hooks/useFetch";
 import Graph from "./Graph";
-import Data from "./data/Data";
 import { Typography } from "@mui/material";
 
 export default function Home() {
-  const { po, receipt, transfer, invoice } = useFetch(
+  const { po, receipt, transfer, invoice, loading } = useFetch(
     `${window.API_BASE_URL}/dashboard`
   );
-  console.log(po, receipt, transfer, invoice);
+  po.total = po.length;
+  po.id = "Purchase Orders";
+  receipt.total = receipt.length;
+  receipt.id = "Receipts";
+  transfer.total = transfer.length;
+  transfer.id = "Transfers";
+  invoice.total = invoice.length;
+  invoice.id = "Invoices";
 
-  return (
+  return loading ? (
+    "...loading"
+  ) : (
     <div className="dash_page">
       <Typography variant="h5" color="#fff">
         Totals
       </Typography>
       <div className="bar_chart">
-        <Graph data={Data} />
+        <Graph data={[po, receipt, transfer, invoice]} />
       </div>
     </div>
   );
