2171042cc17f19e855a71f1b0f345329d98b6b20
Tim <timevko@gmail.com>
2023-12-17 16:57:29 -0500

    refactor

--- src/js/components/ProductionSchedule.jsx
+++ src/js/components/ProductionSchedule.jsx
@@ -9,6 +9,8 @@ export default function ProductionSchedule() {
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
+  const addHighlightClass = (params, str) => params && params.value !== null && params.value !== undefined && params.value.toString().trim() && str;
+
   return (
     <Tables
       title="Production Schedule"
@@ -24,105 +26,105 @@ export default function ProductionSchedule() {
           headerName: "Shift 3",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-1',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-1'),
         },
         {
           field: "SIDE3",
           headerName: "Side 3",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-1',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-1'),
         },
         {
           field: "DESC3",
           headerName: "Description 3",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-1',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-1'),
         },
         {
           field: "QTY3",
           headerName: "Quantity 3",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-1',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-1'),
         },
         {
           field: "COMMENT3",
           headerName: "Comment",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-1',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-1'),
         },
         {
           field: "SHIFT2",
           headerName: "Shift 2",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-2',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-2'),
         },
         {
           field: "SIDE2",
           headerName: "Side 2",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-2',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-2'),
         },
         {
           field: "DESC2",
           headerName: "Description",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-2',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-2'),
         },
         {
           field: "QTY2",
           headerName: "Quantity 2",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-2',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-2'),
         },
         {
           field: "COMMENT2",
           headerName: "Comment",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-2',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-2'),
         },
         {
           field: "SHIFT1",
           headerName: "Shift 1",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-3',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-3'),
         },
         {
           field: "SIDE1",
           headerName: "Side 1",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-3',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-3'),
         },
         {
           field: "DESC1",
           headerName: "Description",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-3',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-3'),
         },
         {
           field: "QTY1",
           headerName: "Quantity 1",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-3',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-3'),
         },
         {
           field: "COMMENT1",
           headerName: "Comment",
           flex: 1,
           minWidth: 150,
-          cellClassName: (params) => params && params.value !== null && params.value.toString().trim() && 'highlight-3',
+          cellClassName: (params) => addHighlightClass(params, 'highlight-3'),
         },
       ]}
       rows={data.map((obj) => ({ ...obj, id: obj.LINENUM + obj.DESC1 }))}
