c61645423c496e5f3aab9e142815302855f148f2
Tim <timevko@gmail.com>
2024-04-21 16:22:42 -0400

    add columns

--- src/js/components/PO-Search.jsx
+++ src/js/components/PO-Search.jsx
@@ -54,6 +54,86 @@ export default function POSearch() {
         <Tables
           title="Purchase Order:"
           rows={data.map(o => ({ ...o, id: o.PONUMBER }))}
+          columns={[
+            {
+              field: "PONUMBER",
+              headerName: "PO Number",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "DATE",
+              headerName: "Date",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "ITEMNO",
+              headerName: "Item Number",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "ITEMDESC",
+              headerName: "Item Description",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "LOCATION",
+              headerName: "Location",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "ORDERUNIT",
+              headerName: "Order Unit",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "OQORDERED",
+              headerName: "Quantity Ordered",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "OQRECEIVED",
+              headerName: "Quantity Received",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "OQCANCELED",
+              headerName: "Quantity Cancelled",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "OQOUTSTANDD",
+              headerName: "Quantity Outstanding",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "UNITCOST",
+              headerName: "Unit Cost",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "EXTENDED",
+              headerName: "Extended",
+              flex: 1,
+              minWidth: 150,
+            },
+            {
+              field: "RCPNUMBER",
+              headerName: "RCP Number",
+              flex: 1,
+              minWidth: 150,
+            },
+          ]}
         />
       )}
       {!loading && (error || data?.length === 0) && (
