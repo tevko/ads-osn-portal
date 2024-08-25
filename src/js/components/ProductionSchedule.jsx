cd420c6788396c5da47804e169fdb6ed4f2f1d51
Tim <timevko@gmail.com>
2024-08-25 10:41:35 -0400

    fix date formatting func

--- src/js/components/ProductionSchedule.jsx
+++ src/js/components/ProductionSchedule.jsx
@@ -24,7 +24,7 @@ export default function ProductionSchedule() {
         setValues(d.filter(o => {
           const currDatePlusSeven = new Date();
           currDatePlusSeven.setDate(currDatePlusSeven.getDate() + 7);
-          const objDate = new Date(o.FormattedDate);
+          const objDate = new Date(o.DATE);
           return objDate <= currDatePlusSeven && (objDate >= td || (objDate.getDay() === td.getDay() && objDate.getMonth() === td.getMonth() && objDate.getFullYear() === td.getFullYear()));
         }));
       } else {
